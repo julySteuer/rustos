625463324a5e746b12cb8787dc6ae2a02d3d734e
ToastBreadMan <xghostx2709@gmail.com>
2021-11-24 15:27:11 +0100

    panick commit

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,8 @@
-// src/main.rs
-
-#![no_std] // don't link the Rust standard library
-#![no_main] // disable all Rust-level entry points
+#![no_std] 
+#![no_main]
 
 use core::panic::PanicInfo;
 
-/// This function is called on panic.
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
   loop {}
@@ -17,12 +14,17 @@ static HELLO: &[u8] = b"Hello World!";
 pub extern "C" fn _start() -> ! {
     
     let vga_buffer = 0xb8000 as *mut u8;
+    let vidmem = 0xa000 as *mut u8;
+            unsafe {
+                *vidmem.offset(0) = 0x0F;
+            }  
 
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+
+    //for (i, &byte) in HELLO.iter().enumerate() {
+    //    unsafe {
+    //        *vga_buffer.offset(i as isize * 2) = byte;
+    //        *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+    //    }
+    //}
     loop {}
 }
